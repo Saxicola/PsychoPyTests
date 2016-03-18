572448b78a3a473cd37ada0c8e07813b6ead7ab5
KamilVlcek <kamil@biomed.cas.cz>
2016-03-18 12:40:35 +0100

    LPTtest - novy skript na otestovani odchylek v casovani LPT portu
    - po 6ti framech = 100 ms posle strobe on
    - po dalsich 6ti framech posle strobe off
    - tak 300x dokola

---
+++ LPTtest/LPTtest.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This experiment was created using PsychoPy2 Experiment Builder (v1.80.03), 2016_03_18_1237
+If you publish work using this script please cite the relevant PsychoPy publications
+  Peirce, JW (2007) PsychoPy - Psychophysics software in Python. Journal of Neuroscience Methods, 162(1-2), 8-13.
+  Peirce, JW (2009) Generating stimuli for neuroscience using PsychoPy. Frontiers in Neuroinformatics, 2:10. doi: 10.3389/neuro.11.010.2008
+"""
+
+from __future__ import division  # so that 1/3=0.333 instead of 1/3=0
+from psychopy import visual, core, data, event, logging, sound, gui
+from psychopy.constants import *  # things like STARTED, FINISHED
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import sin, cos, tan, log, log10, pi, average, sqrt, std, deg2rad, rad2deg, linspace, asarray
+from numpy.random import random, randint, normal, shuffle
+import os  # handy system and path functions
+
+#kamil =====================================
+from ctypes import windll 
+pport = windll.inpout32
+pport_addrr = 0x2FF8 #0x0378 - pocitac #0x2FF8 - notebook
+pport.Out32(pport_addrr, 4) # sets pin no.3 to high
+pport.Out32(pport_addrr+2, 0) # strobe off
+#port reset
+pport.Out32(pport_addrr, 4) # sets pin no.3 to high
+pport.Out32(pport_addrr+2, 0) # strobe off
+
+# Store info about the experiment session
+expName = u'LPTtest'  # from the Builder filename that created this script
+expInfo = {u'participant': u''}
+dlg = gui.DlgFromDict(dictionary=expInfo, title=expName)
+if dlg.OK == False: core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+
+# Setup filename for saving
+filename = 'data/%s_%s_%s' %(expInfo['participant'], expName, expInfo['date'])
+
+# An ExperimentHandler isn't essential but helps with data saving
+thisExp = data.ExperimentHandler(name=expName, version='',
+    extraInfo=expInfo, runtimeInfo=None,
+    originPath=None,
+    savePickle=True, saveWideText=True,
+    dataFileName=filename)
+#save a log file for detail verbose info
+logFile = logging.LogFile(filename+'.log', level=logging.EXP)
+logging.console.setLevel(logging.WARNING)  # this outputs to the screen, not a file
+
+endExpNow = False  # flag for 'escape' or other condition => quit the exp
+
+# Start Code - component code to be run before the window creation
+
+# Setup the Window
+win = visual.Window(size=(1280, 1024), fullscr=True, screen=0, allowGUI=False, allowStencil=False,
+    monitor=u'testMonitor', color=[0,0,0], colorSpace=u'rgb',
+    blendMode=u'avg', useFBO=True,
+    )
+# store frame rate of monitor if we can measure it successfully
+expInfo['frameRate']=win.getActualFrameRate()
+if expInfo['frameRate']!=None:
+    frameDur = 1.0/round(expInfo['frameRate'])
+else:
+    frameDur = 1.0/60.0 # couldn't get a reliable measure so guess
+
+# Initialize components for Routine "trial"
+trialClock = core.Clock()
+ISI = core.StaticPeriod(win=win, screenHz=expInfo['frameRate'], name='ISI')
+krizek = visual.TextStim(win=win, ori=0, name='krizek',
+    text=u'+',    font=u'Arial',
+    pos=[0, 0], height=0.1, wrapWidth=None,
+    color=u'white', colorSpace=u'rgb', opacity=1,
+    depth=-1.0)
+
+# Create some handy timers
+globalClock = core.Clock()  # to track the time since experiment started
+routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
+
+# set up handler to look after randomisation of conditions etc
+trials = data.TrialHandler(nReps=300, method=u'sequential', 
+    extraInfo=expInfo, originPath=None,
+    trialList=[None],
+    seed=None, name='trials')
+thisExp.addLoop(trials)  # add the loop to the experiment
+thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
+# abbreviate parameter names if possible (e.g. rgb=thisTrial.rgb)
+if thisTrial != None:
+    for paramName in thisTrial.keys():
+        exec(paramName + '= thisTrial.' + paramName)
+
+for thisTrial in trials:
+    currentLoop = trials
+    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+    if thisTrial != None:
+        for paramName in thisTrial.keys():
+            exec(paramName + '= thisTrial.' + paramName)
+    
+    #------Prepare to start Routine "trial"-------
+    t = 0
+    trialClock.reset()  # clock 
+    frameN = -1
+    # update component parameters for each repeat
+    # keep track of which components have finished
+    trialComponents = []
+    trialComponents.append(ISI)
+    trialComponents.append(krizek)
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    
+    #-------Start Routine "trial"-------
+    continueRoutine = True
+    while continueRoutine:
+        # get current time
+        t = trialClock.getTime()
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        # *krizek* updates
+        if frameN >= 6 and krizek.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            krizek.tStart = t  # underestimates by a little under one frame
+            krizek.frameNStart = frameN  # exact frame index
+            krizek.setAutoDraw(True)
+            #kamil
+            pport.Out32(pport_addrr, 255) # sets all pins to low
+            pport.Out32(pport_addrr+2, 1) # strobe on
+            
+        elif krizek.status == STARTED and frameN >= 12:
+            krizek.setAutoDraw(False)
+            #kamil
+            pport.Out32(pport_addrr, 4) # sets pin no.3 to high
+            pport.Out32(pport_addrr+2, 0) # strobe off
+            
+        # *ISI* period
+        if frameN >= 0 and ISI.status == NOT_STARTED:
+            # keep track of start time/frame for later
+            ISI.tStart = t  # underestimates by a little under one frame
+            ISI.frameNStart = frameN  # exact frame index
+            ISI.start((6-frameN)*frameDur)
+        elif ISI.status == STARTED: #one frame should pass before updating params and completing
+            ISI.complete() #finish the static period
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            routineTimer.reset()  # if we abort early the non-slip timer needs reset
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in trialComponents:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # check for quit (the Esc key)
+        if endExpNow or event.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+        else:  # this Routine was not non-slip safe so reset non-slip timer
+            routineTimer.reset()
+    
+    #-------Ending Routine "trial"-------
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    thisExp.nextEntry()
+    
+# completed 300 repeats of 'trials'
+
+win.close()
+core.quit()
